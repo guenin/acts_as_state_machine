629d3032568680e5e0411e0899d766611cd49dfa
John Guenin <john@guen.in>
2008-11-04 00:47:41 -0800

    Fix count condition syntax.  All tests finally pass with Rails 2.1.  Huzzah!

--- test/acts_as_state_machine_test.rb
+++ test/acts_as_state_machine_test.rb
@@ -191,15 +191,15 @@ class ActsAsStateMachineTest < Test::Unit::TestCase
   end
   
   def test_count_in_state
-    cnt0 = Conversation.count(['state_machine = ?', 'read'])
+    cnt0 = Conversation.count(:conditions => ['state_machine = ?', 'read'])
     cnt  = Conversation.count_in_state(:read)
     
     assert_equal cnt0, cnt
   end
   
   def test_count_in_state_with_conditions
-    cnt0 = Conversation.count(['state_machine = ? AND subject = ?', 'read', 'Foo'])
-    cnt  = Conversation.count_in_state(:read, ['subject = ?', 'Foo'])
+    cnt0 = Conversation.count(:conditions => ['state_machine = ? AND subject = ?', 'read', 'Foo'])
+    cnt  = Conversation.count_in_state(:read, :conditions => ['subject = ?', 'Foo'])
     
     assert_equal cnt0, cnt
   end
